1416df126411486cee612ac63da68f703a485437
Robert Kulp <pedalinon@gmail.com>
2020-03-18 13:04:14 -0700

    user routes working. Middleware next

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api/posts', userRouter);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -8,35 +8,94 @@ router.post('/', (req, res) => {
   console.log(req.body)
   User.insert(req.body)
   .then(user => {
-    res.status(200).json(post);
+    res.status(200).json(user);
   })
   .catch(error => {
     res.status(500).json({ error: '500, server error!' });
   });
 });
 
+
+// What is this supposed to do? 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
+  const postText = req.body.text;
+  const userId = req.params.id;
+
+  User.getById(postText)
+  .then(post => {
+    res.status(200).json({ success: `${postText} has been posted to user id ${userId}` })
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  User.get()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const userId = req.params.id;
+
+   User.getById(userId)
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  console.log(req.params.id);
+  const userId = req.params.id;
+
+  User.getUserPosts(userId)
+  .then(userPosts => {
+    res.status(200).json(userPosts)
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  console.log(req.params);
+  const userId = req.params.id;
+
+  User.remove(userId)
+  .then(user => {
+    res.status(200).json({ success: 'The user is gone!' })
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  console.log(req.params, req.body);
+  const userId = req.params.id;
+  const changes = req.body;
+
+  User.update(userId, changes)
+  .then(user => {
+    res.status(200).json({ success: 'The user has been updated!' })
+  })
+  .catch(error => {
+    res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 //custom middleware
