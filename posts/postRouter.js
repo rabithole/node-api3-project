0499233b4e8b8b7e2cf674c44087dad6d4b5a01b
Robert Kulp <pedalinon@gmail.com>
2020-03-18 15:58:19 -0700

    All routes working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,60 @@
 const express = require('express');
+const Posts = require('./postDb.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.get()
+  .then(post => {
+  	res.status(200).json(post);
+  })
+  .catch(error => {
+  	res.status(500).json({ error: '500, server error!' });
+  });
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  console.log(req.params);
+  const userId = req.params.id;
+
+  Posts.getById(userId)
+  .then(post => {
+  	res.status(200).json(post);
+  })
+  .catch(error => {
+  	res.status(500).json({ error: '500, server error' })
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  console.log(req.params, req.body);
+  const postId = req.params.id;
+
+  Posts.remove(postId)
+  .then(post => {
+  	res.status(200).json(post);
+  })
+  .catch(error => {
+  	res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  console.log(req.params, req.body);
+  const postId = req.params.id;
+  const changes = req.body;
+
+  Posts.update(postId, changes)
+  .then(post => {
+  	res.status(200).json(post);
+  })
+  .catch(error => {
+  	res.status(500).json({ error: '500, server error!' })
+  })
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
